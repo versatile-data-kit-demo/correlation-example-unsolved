be8714496eb41b019485b09346b1301e9801ee8b
Antoni Ivanov <aivanov@vmware.com>
2022-10-14 21:43:46 +0300

    Update 04_create_weekly_correlation.sql

--- sample scripts/04_create_weekly_correlation.sql
+++ sample scripts/04_create_weekly_correlation.sql
@@ -1,11 +1,6 @@
 /* Create a table that will store the weekly aggreagted data and correlation coefficients.
-Table naming convention: weekly_correlation_YOURNAME where replace "YOURNAME" with your name.
+-- Table naming convention: prefix + _ + weekly_correlation 
 
-Construct the CREATE TABLE statement by yourself with the following columns and data types:
- - date with type VARCHAR
- - num_no_scent_reviews with type INTEGER: this will contain the number of "no scent" reviews for the week
- - number_of_covid_cases_weekly with type INTEGER: this will contain the number of weekly covid cases
- - correlation_coeff with type REAL: this will contain the correlation coefficients for the given week */
  
 CREATE TABLE IF NOT EXISTS /*!!! ENTER THE NAME OF THE TABLE HERE !!!*/ (
     date VARCHAR,
