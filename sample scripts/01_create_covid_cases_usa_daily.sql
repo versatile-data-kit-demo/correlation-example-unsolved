0937da19b6790d12fe8a2e0e68273cad990eb4df
Antoni Ivanov <aivanov@vmware.com>
2022-10-14 21:42:40 +0300

    Update 01_create_covid_cases_usa_daily.sql

--- sample scripts/01_create_covid_cases_usa_daily.sql
+++ sample scripts/01_create_covid_cases_usa_daily.sql
@@ -1,5 +1,5 @@
 -- Create a table that will store the daily COVID-19 cases in the US with 2 columns: date and number of cases.
--- Table naming convention: covid_cases_usa_daily_YOURNAME where replace "YOURNAME" with your name.
+-- Table naming convention: prefix + _ + covid_cases_usa_daily 
 
 CREATE TABLE IF NOT EXISTS /*!!! ENTER THE NAME OF THE TABLE HERE !!!*/ (
     obs_date VARCHAR,
