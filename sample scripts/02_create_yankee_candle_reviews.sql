068539f805c54f30ead252da36827cc5f73c75ef
Antoni Ivanov <aivanov@vmware.com>
2022-10-14 21:42:58 +0300

    Update 02_create_yankee_candle_reviews.sql

--- sample scripts/02_create_yankee_candle_reviews.sql
+++ sample scripts/02_create_yankee_candle_reviews.sql
@@ -1,5 +1,6 @@
 -- Create a table that will store the Amazon Yankee candle reviews with 2 columns: date and review
--- Table naming convention: yankee_candle_reviews_YOURNAME where replace "YOURNAME" with your name.
+-- Table naming convention: prefix + _ + yankee_candle_reviews 
+
 
 CREATE TABLE IF NOT EXISTS /*!!! ENTER THE NAME OF THE TABLE HERE !!!*/ (
     Date VARCHAR,
